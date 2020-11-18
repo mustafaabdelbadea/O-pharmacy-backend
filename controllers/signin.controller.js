707d0189314c6b30f2a9f0ebcf1e6f57c61dc0d3
mustafa <mustafa.abdelbadea@gmail.com>
2020-11-18 15:03:24 +0200

    install express-validator bcrypt

---
+++ controllers/signin.controller.js
@@ -0,0 +1,14 @@
+//import models 
+// const pharmacyModel=require('../models/index.model');
+// const customerModel=require('../models/notes.model');
+
+
+module.exports.pharmacySignin=async (req,res)=>{
+// let {email,password}=req.body;
+// let user=await pharmacyModel.find
+}
+
+
+module.exports.customerSignin=async(req,res)=>{
+
+}
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,10 @@
-const indexRouter = require('express').Router()
+const indexRouter = require('express').Router();
 const signinController=require ('../controllers/signin.controller');
 
 
+indexRouter.post('/pharmacySignin',signinController.pharmacySignin);
+indexRouter.post('/customerSignin',signinController.customerSignin);
 
 
 
-
-module.exports = indexRouter
+module.exports = indexRouter;
