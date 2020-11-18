8a7aa29cef6a1dfc42123cc483dd76b0179735eb
mustafa <mustafa.abdelbadea@gmail.com>
2020-11-18 19:25:57 +0200

    auth commit

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt =require('jsonwebtoken')
+module.exports=(req,res,next)=>{
+//send token in header 
+const token=req.header('token');
+//check if token is exists and token is right
+if(token&&token!=null&&token!=undefined){
+jwt.verify(token,'pharmjwt',(err,decoded)=>{
+    if(err){
+         res.json('authentication fallid or error in token');
+    }
+    else{
+        next()
+    }
+})
+}
+else{
+     res.json('error in token or token not provided');
+}
+}
