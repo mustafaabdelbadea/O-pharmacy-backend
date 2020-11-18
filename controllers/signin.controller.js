a4595f4aa105d4371f1c93ff9cebf2b982b9e5d4
mustafaabdelbadea <mustafa.abdelbadea@gmail.com>
2020-11-18 22:24:23 +0200

    signin add models

--- controllers/signin.controller.js
+++ controllers/signin.controller.js
@@ -1,14 +1,86 @@
+const bcrypt=require('bcrypt');
+const jwt =require('jsonwebtoken')
 //import models 
-// const pharmacyModel=require('../models/index.model');
-// const customerModel=require('../models/notes.model');
-
-
+const pharmaciesModel=require('../models/pharmacies.model');
+const customerModel=require('../models/customer.model');
 module.exports.pharmacySignin=async (req,res)=>{
-// let {email,password}=req.body;
-// let user=await pharmacyModel.find
+let {email,password}=req.body;        
+    let pharmacies=await pharmaciesModel.findOne({email});
+        if(pharmacies){
+            //check if email is verified or not 
+            if(pharmacies.isVerified==false){
+                res.json({message:"email not Verified"});
+           }
+           // check hased password
+           const match =await bcrypt.compare(password,pharmacies.password)
+            if(match){
+                jwt.sign(
+                    //retrieve in token 
+                    {_id:pharmacies._id,name:pharmacies.name,password:pharmacies.password},
+                    //secret key pharmjwt
+                    "pharmjwt",
+                    //send token in header
+                    (err,token)=>{
+                        res.header('token',token).json(
+                            {
+                                name:pharmacies.name
+                            }
+                        )
+                    }
+                )
+                // res.json({msg:"success"});
+            }
+            else{
+                res.json({msg:"Invalid email or password"});
+            }
+        }
+        else{
+             res.json({msg:"Invalid email or password"});
+        }
+}
+module.exports.customerSignin=async(req,res)=>{
+    let {email,password}=req.body;        
+        let customers=await customerModel.findOne({email});
+            if(customers){
+                //check if email is verified or not 
+                if(customers.isVerified==false){
+                    res.json({message:"email not Verified"});
+               }
+               // check hased password
+               const match =await bcrypt.compare(password,customers.password)
+                if(match){
+                    jwt.sign(
+                        //retrieve in token 
+                        {_id:customers._id,name:customers.name,password:customers.password},
+                        //secret key pharmjwt
+                        "pharmjwt",
+                        //send token in header
+                        (err,token)=>{
+                            res.header('token',token).json(
+                                {
+                                    name:customers.name
+                                }
+                            )
+                        }
+                    )
+                    // res.json({msg:"success"});
+                }
+                else{
+                    res.json({msg:"Invalid email or password"});
+                }
+            }
+            else{
+                 res.json({msg:"Invalid email or password"});
+            }
 }
 
-
-module.exports.customerSignin=async(req,res)=>{
-
+// module.exports.check=async(req,res)=>{
+//     jwt.verify(req.params.token,'checkEmail',async(err,decoded)=>{
+//        let user=await userModel.findOneAndUpdate({email:decoded},{confirmed:true})
+// res.redirect('/');
+//     });
+// }
+module.exports.home=async(req,res)=>{
+    let customers=await customerModel.findOne({});
+     res.json(customers);
 }
