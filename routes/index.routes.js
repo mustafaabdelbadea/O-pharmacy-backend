18fb1da2dbfa0c07c3e4004b9375af2fa7670b13
mustafa <mustafa.abdelbadea@gmail.com>
2020-11-18 13:57:46 +0200

    create routes fiile

---
+++ routes/index.routes.js
@@ -0,0 +1,8 @@
+const indexRouter = require('express').Router()
+const signinController=require ('../controllers/signin.controller');
+
+
+
+
+
+module.exports = indexRouter
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser=require('body-parser').urlencoded({extended:false});
 const path=require('path');
 const mongoose=require('mongoose');
 const cors=require('cors');
+const indexRoute = require('./routes/index.routes')
 app.use(cors({}));
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
@@ -11,7 +12,7 @@ app.use(express.static(path.join(__dirname,'assets')));
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 app.use(bodyParser);
 mongoose.set('useFindAndModify',false);
-
+app.use(indexRoute);
 mongoose.connect("mongodb://localhost:27017/o-pharmacyDB", {  useNewUrlParser: true,useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true)
 
