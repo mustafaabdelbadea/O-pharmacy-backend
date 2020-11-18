d16aff40dae648f102de14f8e70f67b02ebbb35b
mustafaabdelbadea <mustafa.abdelbadea@gmail.com>
2020-11-18 21:57:56 +0200

    routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 const indexRouter = require('express').Router();
 const signinController=require ('../controllers/signin.controller');
-
+const verifyToken=require('../middlewares/auth');
+indexRouter.get('/',signinController.home);
 
 indexRouter.post('/pharmacySignin',signinController.pharmacySignin);
 indexRouter.post('/customerSignin',signinController.customerSignin);
