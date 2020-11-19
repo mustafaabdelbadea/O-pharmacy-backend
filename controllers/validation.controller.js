8ead5bd682d55a3895f8f431a4881805f1ca6d7f
mustafaabdelbadea <mustafa.abdelbadea@gmail.com>
2020-11-19 20:31:34 +0200

    confirm password validation

--- controllers/validation.controller.js
+++ controllers/validation.controller.js
@@ -5,10 +5,10 @@ module.exports.signupValidation=[
     check('name','invalid input').matches(/[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/),
     check('email','invalid input').isEmail(),
     check('password','invalid input').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
-    // check('confirmPassword','invalid input').custom((value, { req }) => {
-    //     if (value !== req.body.password) {
-    //       throw new Error('Password confirmation does not match password');
-    //     }
-    //     return true;
-    //   })
+    check('confirmPassword','invalid input').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Password confirmation does not match password');
+        }
+        return true;
+      })
 ]
