9558a2314b24fbc8831177866d09a3bb6d5ab2ef
abdallhmz99 <abdallhmz99@gmail.com>
2020-12-14 17:19:28 +0200

    edit pass by token

--- controllers/editData.controler.js
+++ controllers/editData.controler.js
@@ -3,15 +3,26 @@ const jwt = require('jsonwebtoken');
 const { check, validationResult } = require("express-validator");
 const customersModel = require("../models/customer.model");
 const pharmaciesModel = require("../models/pharmacies.model");
+const mongoose = require('mongoose');
+
+
 
 
 module.exports.edit_Pharmacy_password = async (req, res) => {
-  
+    const token = req.header('token');
+
+    jwt.verify(token, 'pharmjwt', async(err, decoded) => {
+        if (err) {
+            res.json('error in token');
+        }
+        else {
+           const id=decoded._id
+           
     const errors = validationResult(req); //check input validation
   
     const {oldpassword, password ,confirmPassword} = req.body; // assing data in request into variabls
     
-    const user = await pharmaciesModel.find({_id:req.params.id}); //search for the pharmacy by id in url
+    const user = await pharmaciesModel.find({_id:id}); //search for the pharmacy by id in url
  
 if(user){ // user is exist 
    
@@ -23,7 +34,7 @@ if(user){ // user is exist
            try{
       
             const hashPassword = bcrypt.hashSync(password, 8); //make password hashed 
-            await pharmaciesModel.findOneAndUpdate({_id:req.params.id},{password:hashPassword}); //save new password
+            await pharmaciesModel.findOneAndUpdate({_id:id},{password:hashPassword}); //save new password
             res.json({ message: "password edited successfully"}); 
         }
 
@@ -44,50 +55,65 @@ if(user){ // user is exist
      {
         res.json({ message: "Id is not exist"});
      }
+        }
+    })
+  
   
   
   
     };
 
 module.exports.edit_customer_password = async (req, res) => {
-  
-        const errors = validationResult(req); //check input validation
+
+    const token = req.header('token');
+
+    jwt.verify(token, 'pharmjwt',async(err, decoded) => {
+        if (err) {
+            res.json({massage : "error in token"});
+        }
+        else {
+            const id =decoded._id
+            const errors = validationResult(req); //check input validation
       
-        const {oldpassword, password ,confirmPassword} = req.body; // assing data in request into variabls
+            const {oldpassword, password ,confirmPassword} = req.body; // assing data in request into variabls
+            
+            const user = await customersModel.find({_id:id}); //search for the pharmacy by id in url
+         
+        if(user){ // user is exist 
+           
+            const match = await bcrypt.compare(oldpassword , user[0].password); //check input validation
+            
+                if (match) { //old pass is match
+                  if (errors.isEmpty()) { //no validaion error
+                    
+                   try{
+              
+                    const hashPassword = bcrypt.hashSync(password, 8); //make password hashed 
+                    await customersModel.findOneAndUpdate({_id:id},{password:hashPassword}); //save new password
+                    res.json({ message: "password edited successfully"}); 
+                }
         
-        const user = await customersModel.find({_id:req.params.id}); //search for the pharmacy by id in url
-     
-    if(user){ // user is exist 
-       
-        const match = await bcrypt.compare(oldpassword , user[0].password); //check input validation
+                   catch (e) {// print error if it exist
+                        res.json(e)
+                      }
         
-            if (match) { //old pass is match
-              if (errors.isEmpty()) { //no validaion error
-                
-               try{
-          
-                const hashPassword = bcrypt.hashSync(password, 8); //make password hashed 
-                await customersModel.findOneAndUpdate({_id:req.params.id},{password:hashPassword}); //save new password
-                res.json({ message: "password edited successfully"}); 
-            }
-    
-               catch (e) {// print error if it exist
-                    res.json(e)
+                 }
+                 else { //there is validaion error
+                    res.json({ message: errors.array()});
                   }
-    
+          
+                }else //old password is not match
+                {  
+                      res.json({ message: "old password is not match"});
+                }
+            }else //pharmcay_id is not found
+             {
+                res.json({ message: "Id is not exist"});
              }
-             else { //there is validaion error
-                res.json({ message: errors.array()});
-              }
-      
-            }else //old password is not match
-            {  
-                  res.json({ message: "old password is not match"});
-            }
-        }else //pharmcay_id is not found
-         {
-            res.json({ message: "Id is not exist"});
-         }
+        }
+    })
+  
+
       
       
       
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -29,8 +29,8 @@ indexRouter.post('/forgotPasswordPharmacy',forgotPasswordController.forgotPasswo
 indexRouter.post('/pharmacyForgotPassword/:token',forgotPasswordValidation,forgotPasswordTokenController.pharmacyForgotPassword)
 indexRouter.post('/medicalhistory',authMiddleware, medicalhistoryController.medicalhistory );
 indexRouter.get('/medicalhistoryRetrieve',authMiddleware, medicalhistoryController.retrieveData);
-indexRouter.post('/editPharmacyPass/:id',authMiddleware,validation.editPassValidation,editController.edit_Pharmacy_password);
-indexRouter.post('/editCustomerPass/:id',authMiddleware,validation.editPassValidation,editController.edit_customer_password);
+indexRouter.post('/editPharmacyPass',authMiddleware,validation.editPassValidation,editController.edit_Pharmacy_password);
+indexRouter.post('/editCustomerPass',validation.editPassValidation,editController.edit_customer_password);
 
 indexRouter.get('*',notfoundController.notfound )
 //validation.signupValidation,
