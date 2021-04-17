0f6f39193574a1c81fd07442df29ef8cc4a83b07
abdallhmz99 <abdallhmz99@gmail.com>
2021-04-17 14:24:52 +0200

    Verify mail

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -66,18 +66,43 @@ module.exports.pharmacySignup = async (req, res) => {
 
               // send mail with defined transport object
               let info = await transporter.sendMail({
-                from: '"Fred Foo" <3bdallhmz99@gmail.com>', // sender address
+                from: '"O-Pharmacy" <OpharmacyTeam@gmail.com>', // sender address
                 to: req.body.email, // list of receivers
-                subject: "Hello ✔", // Subject line
-                text: "Hello world?", // plain text body
+                subject: "Verify mail", // Subject line
+                text: "Verify your mail", // plain text body
                 html: `
         
-          <div style="background-color:#000;color:#fff; padding:100px">
-          
-        <h1 style="margin:50px"> <a href="http://localhost:4200/pharmacyVerifyEmail/${token}">click to confirm</a>  </h1>
-          
-          </div>
-      
+                <html>
+                <head>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                </head>
+                <style>
+                 
+                </style>
+            <body>
+            
+                <!-- style="background-color: #f3f8ff;"  -->
+                <div class=" bg-dark w-50 mt-2 m-auto text-center p-3 rounded">
+                    
+                    <img class="mb-2" style="width: 10rem; height: 10rem;" src="./checkmark-for-verification (1).svg" alt=""><br>
+                    <h3 class="text-white mb-4" >Verify this email address</h3>
+            
+                    <a type="button" class="btn btn-success mb-4" href="http://localhost:4200/pharmacyVerifyEmail/${token}">verify</a>
+            
+                    <p class="text-white mb-4"> Thank you for joining us. <br> <span style="font-size: large; font-weight: bold;">O-Pharmacy</span> hope you enjoy your account.</p>
+                      
+                </div>
+                
+            
+            
+                
+            
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            
+            </body>
+            </html>
       
           `, // html body
 
@@ -175,18 +200,43 @@ module.exports.customerSignup = async (req, res) => {
 
                 // send mail with defined transport object
                 let info = await transporter.sendMail({
-                  from: '"Fred Foo" <3bdallhmz99@gmail.com>', // sender address
+                  from: '"O-Pharmacy" <OpharmacyTeam@gmail.com>', // sender address
                   to: req.body.email, // list of receivers
-                  subject: "Hello ✔", // Subject line
-                  text: "Hello world?", // plain text body
+                  subject: "Verify mail", // Subject line
+                  text: "Verify your mail", // plain text body
                   html: `
          
-           <div style="background-color:#000;color:#fff; padding:100px">
-           
-         <h1 style="margin:50px"> <a href="http://localhost:4200/customerVerifyEmail/${token}">click to confirm</a>  </h1>
-           
-           </div>
-       
+                  <html>
+                  <head>
+                      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                  </head>
+                  <style>
+                   
+                  </style>
+              <body>
+              
+                  <!-- style="background-color: #f3f8ff;"  -->
+                  <div class=" bg-dark w-50 mt-2 m-auto text-center p-3 rounded">
+                      
+                      <img class="mb-2" style="width: 10rem; height: 10rem;" src="./checkmark-for-verification (1).svg" alt=""><br>
+                      <h3 class="text-white mb-4" >Verify this email address</h3>
+              
+                      <a type="button" class="btn btn-success mb-4" href="http://localhost:4200/pharmacyVerifyEmail/${token}">verify</a>
+              
+                      <p class="text-white mb-4"> Thank you for joining us. <br> <span style="font-size: large; font-weight: bold;">O-Pharmacy</span> hope you enjoy your account.</p>
+                        
+                  </div>
+                  
+              
+              
+                  
+              
+                  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+              
+              </body>
+              </html>       
        
            `, // html body
 
